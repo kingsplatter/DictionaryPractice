bd69e1a50ef34c3f8dbfd4376f4f49ae75736a69
kingsplatter <andrew.strange@nwktc.edu>
2015-01-13 07:52:14 -0700

    Start Files
    Initial Push

---
+++ dictionaryDemo.js
@@ -0,0 +1,47 @@
+var data = {"Small Guns":{
+        "Pistols":[
+            {"name":".32 Pistol","DPS":18,"Weight":2},
+            {"name":"10mm Pistol","DPS":54,"Weight":3},
+            {"name":".44 Magnum","DPS":79,"Weight":4}
+        ],
+        "Rifles":[
+            {"name":"AR15","DPS":64,"Weight":7},
+            {"name":"BB Gun","DPS":3,"Weight":2},
+            {"name":"AK47","DPS":88,"Weight":8},
+            {"name":"Hunting Rifle","DPS":19,"Weight":6}
+        ]
+    },
+    "Big Guns":{
+        "Direct Fire":[
+            {"name":"Drone Cannon","DPS":467,"Weight":18},
+            {"name":"Flame Thrower","DPS":130,"Weight":15},
+            {"name":"Gatling Laser","DPS":160,"Weight":18},
+            {"name":"Heavy Incinerator","DPS":328,"Weight":15},
+            {"name":"Minigun","DPS":100,"Weight":18}
+        ],
+        "AOE":[
+            {"name":"Fat Man","DPS":433,"Weight":30},
+            {"name":"Missile Launcher","DPS":360,"Weight":20}
+        ]
+    },
+    "Melee":{
+        "Edged":[
+            {"name":"Axe","DPS":29,"Weight":6},
+            {"name":"Officer's Sword","DPS":23,"Weight":3},
+            {"name":"Combat Knife","DPS":21,"Weight":1},
+            {"name":"Shovel","DPS":17,"Weight":3}
+        ],
+        "Blunt":[
+            {"name":"Baseball Bat","DPS":13,"Weight":3},
+            {"name":"Lead Pipe","DPS":21,"Weight":3},
+            {"name":"Nail Board","DPS":11,"Weight":4},
+            {"name":"Pool Cue","DPS":4,"Weight":1},
+            {"name":"Super Sledge","DPS":36,"Weight":20}
+        ],
+        "Unarmed":[
+            {"name":"Brass Knuckles","DPS":10,"Weight":1},
+            {"name":"Spiked Knuckles","DPS":14,"Weight":1},
+            {"name":"Power Fist","DPS":22,"Weight":6}
+        ]
+    }
+}
